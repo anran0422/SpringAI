0f65728de8be1180e1ff01e43c9e0e9915a53ec1
macbook <0422anran@gmail.com>
2025-03-19 18:57:40 +0800

    websocket and disruptor finished

--- CloudLibrary-fronted/src/pages/picture/AddPicturePage.vue
+++ CloudLibrary-fronted/src/pages/picture/AddPicturePage.vue
@@ -36,7 +36,8 @@
           :imageUrl="picture?.url"
           :picture="picture"
           :spaceId="spaceId"
-          :onSuccess="onCropSuccess"
+          :space="space"
+          :onSuccess="onSuccess"
         />
         <ImageOutPainting
           ref="imageOutPaintingRef"
@@ -84,7 +85,7 @@
 
 <script setup lang="ts">
 import PictureUpload from '@/components/PictureUpload.vue'
-import { computed, h, onMounted, reactive, ref } from 'vue'
+import { computed, h, onMounted, reactive, ref, watchEffect } from 'vue'
 import { message } from 'ant-design-vue'
 import { useRoute, useRouter } from 'vue-router'
 import { editPictureUsingPost, getPictureVoByIdUsingGet, listPictureTagCategoryUsingGet } from '@/api/pictureController'
@@ -92,6 +93,7 @@ import UrlPictureUpload from '@/components/UrlPictureUpload.vue'
 import ImageCropper from '@/components/ImageCropper.vue'
 import { EditOutlined,FullscreenOutlined } from '@ant-design/icons-vue'
 import ImageOutPainting from '@/components/ImageOutPainting.vue'
+import { getSpaceVoByIdUsingGet } from '@/api/spaceController'
 
 
 // 空间 id
@@ -220,6 +222,26 @@ const doImagePainting = () => {
 const onImageOutPaintingSuccess = (newPicture: API.PictureVO) => {
   picture.value = newPicture
 }
+
+// 获取空间信息
+const space = ref<API.SpaceVO>()
+
+// 获取空间信息
+const fetchSpace = async () => {
+  // 获取数据
+  if (spaceId.value) {
+    const res = await getSpaceVoByIdUsingGet({
+      id: spaceId.value,
+    })
+    if (res.data.code === 0 && res.data.data) {
+      space.value = res.data.data
+    }
+  }
+}
+
+watchEffect(() => {
+  fetchSpace()
+})
 
 </script>
 
